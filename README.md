e3b72e7a634be68714e41fb91cfb06fdd3edae9d
ChicDead26 <chicdead26@hotmail.com>
2023-06-04 09:38:22 -0300

    Map Rando 1.0
    First commit for the Super Metroid Map Rando

--- README.md
+++
@@ -1,6 +0,0 @@
-# SauceRelic's Work
-This repository is for projects I've made in my free time. For most of this work, I'm fine with other people modifying it, as long as you give me credit if you redistribute it.
-
-Currently, all that is here are some tracker packages I've made for EmoTracker, which can be found here: https://emotracker.net/
-
--Phillip Hiemenz (SauceRelic)
---
+++ sm_maprando_chicdead26/scripts/init.lua
@@ -0,0 +1,33 @@
+--ScriptHost:LoadScript("scripts/class.lua")
+--ScriptHost:LoadScript("scripts/custom_item.lua")
+--ScriptHost:LoadScript("scripts/toggle.lua")
+--ScriptHost:LoadScript("scripts/optionalPhantoon.lua")
+--ScriptHost:LoadScript("scripts/objectiveSwitch.lua")
+--ScriptHost:LoadScript("scripts/objectiveTest.lua")
+
+--local newItem = ToggleItem("Test", "test", "images/Miniboss3E.png")
+--local newPhantoon = OptionalPhantoon("OptionalPhantoon", "optionalphantoon", "images/Boss3.png")
+--local newObjective = ObjectiveTest("ObjectiveTest", "objectiveSwitch", "images/obj_bosses.png", "images/obj_minibosses.png", "images/obj_metroids.png", newPhantoon)
+--local whatever = StupidTest("Test", "test", "images/Miniboss3E.png")
+--newObjective:
+
+Tracker:AddItems("items/objectives.json")
+Tracker:AddItems("items/objBosses.json")
+Tracker:AddItems("items/objMinibosses.json")
+Tracker:AddItems("items/objMetroids.json")
+Tracker:AddItems("items/difficulty.json")
+Tracker:AddItems("items/itemprogression.json")
+Tracker:AddItems("items/qualityoflife.json")
+Tracker:AddItems("items/items.json")
+Tracker:AddItems("items/bosses.json")
+Tracker:AddItems("items/minibosses.json")
+Tracker:AddItems("items/metroids.json")
+
+--Tracker:AddMaps("maps/maps.json")
+--
+--Tracker:AddLocations("locations/logic.json")
+--Tracker:AddLocations("locations/locations.json")
+
+Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/broadcast.json")
+
