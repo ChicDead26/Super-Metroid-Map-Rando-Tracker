e7a0e335ad13b4d2a1802fda2e9e2414349d7925
ChicDead26 <chicdead26@hotmail.com>
2023-06-08 09:54:23 -0300

    Uploaded some files on accident. Oops.

--- sm_maprando_chicdead26/scripts/init.lua
+++ sm_maprando_chicdead26/scripts/init.lua
@@ -1,13 +1,13 @@
-ScriptHost:LoadScript("scripts/class.lua")
-ScriptHost:LoadScript("scripts/custom_item.lua")
-ScriptHost:LoadScript("scripts/toggle.lua")
-ScriptHost:LoadScript("scripts/optionalPhantoon.lua")
-ScriptHost:LoadScript("scripts/objectiveSwitch.lua")
-ScriptHost:LoadScript("scripts/objectiveTest.lua")
+--ScriptHost:LoadScript("scripts/class.lua")
+--ScriptHost:LoadScript("scripts/custom_item.lua")
+--ScriptHost:LoadScript("scripts/toggle.lua")
+--ScriptHost:LoadScript("scripts/optionalPhantoon.lua")
+--ScriptHost:LoadScript("scripts/objectiveSwitch.lua")
+--ScriptHost:LoadScript("scripts/objectiveTest.lua")
 
-local newItem = ToggleItem("Test", "test", "images/Miniboss3E.png")
-local newPhantoon = OptionalPhantoon("OptionalPhantoon", "optionalphantoon", "images/Boss3.png")
-local newObjective = ObjectiveTest("ObjectiveTest", "objectiveSwitch", "images/obj_bosses.png", "images/obj_minibosses.png", "images/obj_metroids.png", newPhantoon)
+--local newItem = ToggleItem("Test", "test", "images/Miniboss3E.png")
+--local newPhantoon = OptionalPhantoon("OptionalPhantoon", "optionalphantoon", "images/Boss3.png")
+--local newObjective = ObjectiveTest("ObjectiveTest", "objectiveSwitch", "images/obj_bosses.png", "images/obj_minibosses.png", "images/obj_metroids.png", newPhantoon)
 --local whatever = StupidTest("Test", "test", "images/Miniboss3E.png")
 --newObjective:
 
