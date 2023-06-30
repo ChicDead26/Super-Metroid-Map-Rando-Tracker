b00be231d7d2da3cda63dc32ed4658146ab031d7
ChicDead26 <chicdead26@hotmail.com>
2023-06-29 21:01:22 -0300

    1.4 Release
    Autotracker

--- sm_maprando_chicdead26/scripts/init.lua
+++ sm_maprando_chicdead26/scripts/init.lua
@@ -1,15 +1,4 @@
---ScriptHost:LoadScript("scripts/class.lua")
---ScriptHost:LoadScript("scripts/custom_item.lua")
---ScriptHost:LoadScript("scripts/toggle.lua")
---ScriptHost:LoadScript("scripts/optionalPhantoon.lua")
---ScriptHost:LoadScript("scripts/objectiveSwitch.lua")
---ScriptHost:LoadScript("scripts/objectiveTest.lua")
-
---local newItem = ToggleItem("Test", "test", "images/Miniboss3E.png")
---local newPhantoon = OptionalPhantoon("OptionalPhantoon", "optionalphantoon", "images/Boss3.png")
---local newObjective = ObjectiveTest("ObjectiveTest", "objectiveSwitch", "images/obj_bosses.png", "images/obj_minibosses.png", "images/obj_metroids.png", newPhantoon)
---local whatever = StupidTest("Test", "test", "images/Miniboss3E.png")
---newObjective:
+ScriptHost:LoadScript("scripts/settings.lua")
 
 Tracker:AddItems("items/objectives.json")
 Tracker:AddItems("items/objBosses.json")
@@ -28,11 +17,11 @@ Tracker:AddItems("items/chozos.json")
 Tracker:AddItems("items/pirates.json")
 Tracker:AddItems("items/extras.json")
 
---Tracker:AddMaps("maps/maps.json")
---
---Tracker:AddLocations("locations/logic.json")
---Tracker:AddLocations("locations/locations.json")
-
 Tracker:AddLayouts("layouts/tracker.json")
 Tracker:AddLayouts("layouts/broadcast.json")
 
+if _VERSION == "Lua 5.3" then
+    ScriptHost:LoadScript("scripts/autotracking.lua")
+else
+    print("Auto-tracker is unsupported by your tracker version")
+end
